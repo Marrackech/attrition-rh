c964e3a94f2df035c2f93b43ca83134b5705f2ae
Marrackech <linasafe5@gmail.com>
2026-04-16 20:15:42 +0200

    update database schema + logs + main

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import time
 from fastapi import FastAPI, HTTPException
 from app.schemas import EmployeeInput, PredictionOutput
 from app.model import predict
@@ -9,29 +10,68 @@ app = FastAPI(
     version="1.0.0"
 )
 
+
 @app.on_event("startup")
 def startup():
     init_db()
+
 
 @app.get("/")
 def root():
     return {"message": "API Attrition RH — opérationnelle"}
 
+
 @app.get("/health")
 def health():
     return {"status": "ok"}
 
+
 @app.post("/predict", response_model=PredictionOutput)
 def predict_attrition(employee: EmployeeInput):
+
+    start_time = time.time()
+
     try:
-        # 1. Calcul de la prédiction via le modèle
+        # 1. Prédiction ML
         result = predict(employee)
-        
-        # 2. Sauvegarde dans la base de données
-        # On convertit les objets Pydantic en dictionnaires
-        save_prediction(employee.model_dump(), result.model_dump())
-        
+
+        # 2. Temps d'inférence
+        inference_time_ms = (time.time() - start_time) * 1000
+
+        # 3. Log data (monitoring MLOps)
+        log_data = {
+            "inference_time_ms": inference_time_ms,
+            "api_response_time_ms": inference_time_ms,
+            "model_version": "v1.0",
+            "status": "success"
+        }
+
+        # 4. Save DB (employees + predictions + logs)
+        save_prediction(
+            employee.model_dump(),
+            result.model_dump(),
+            log_data
+        )
+
         return result
+
     except Exception as e:
-        # Permet de renvoyer une erreur 500 propre au lieu d'un crash serveur
+
+        # log erreur
+        log_data = {
+            "inference_time_ms": 0,
+            "api_response_time_ms": 0,
+            "model_version": "v1.0",
+            "status": "error"
+        }
+
+        try:
+            save_prediction(employee.model_dump(), {
+                "probabilite_depart": 0,
+                "prediction": 0,
+                "interpretation": "error"
+            }, log_data)
+        except:
+            pass
+
         raise HTTPException(status_code=500, detail=str(e))
